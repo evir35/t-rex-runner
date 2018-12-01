18d7ee190c1cf2faa4f9a853c6013b59623b913f
evir35 <evir35@gmail.com>
2018-12-01 14:47:30 +0900

    init commit

---
+++ Assets/Scripts/BackgroundManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundManager : MonoBehaviour
+{
+    private const int velocity = -7;
+
+    private delegate void State();
+
+    private State state;
+    private Transform transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = Idle;
+        transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        state();
+    }
+
+    void Idle()
+    {
+        // Do not anything...
+    }
+
+    void Move()
+    {
+        transform.position += new Vector3(velocity, 0);
+    }
+
+    public void StartMove()
+    {
+        state = Move;
+    }
+
+    public void StopMove()
+    {
+        state = Idle;
+    }
+}
---
+++ Assets/Scripts/GameManger.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManger : MonoBehaviour
+{
+    private delegate void State();
+   
+    private State state;
+    private RexManager rexManager;
+    private BackgroundManager backgroundManager;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        rexManager = FindObjectOfType(typeof(RexManager)) as RexManager;
+        backgroundManager = FindObjectOfType(typeof(BackgroundManager)) as BackgroundManager;
+        state = Ready;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        state();
+    }
+
+    void Ready()
+    {
+        if (Input.anyKey)
+        {
+            Debug.Log("GameState change ReadyState to RunState");
+            state = Run;
+        }
+    }
+
+    void Run()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rexManager.InputJump();
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("start to move background");
+            backgroundManager.StartMove();
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Debug.Log("stop to move background");
+            backgroundManager.StopMove();
+        }
+    }
+}
